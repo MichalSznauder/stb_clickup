939ca48bb9ef19c25944d66fd64bb3f1bd3c51fc
MichalSznauder <100240910+MichalSznauder@users.noreply.github.com>
2023-07-06 21:58:18 +0200

    Update ClickUpProperties.java

--- src/main/java/pl/akademiaqa/properties/ClickUpProperties.java
+++ src/main/java/pl/akademiaqa/properties/ClickUpProperties.java
@@ -10,13 +10,19 @@ public class ClickUpProperties {
     private static final String  TEAM_ID = "clickup.team.id";
 
     public static String getToken() {
+        if (getProperty(TOKEN).isEmpty() || getProperty(TOKEN).startsWith("YOUR")){
+            return System.getProperty("TOKEN");
+        } else {
     return getProperty(TOKEN);
-
+        }
     }
 
     public static String getTeamId() {
+        if(getProperty(TEAM_ID).isEmpty() || getProperty(TEAM_ID).startsWith("YOUR")){
+            return System.getProperty("TEAM_ID");
+        } else {
         return getProperty(TEAM_ID);
-
+        }
     }
 
     private static String getProperty(String key){
